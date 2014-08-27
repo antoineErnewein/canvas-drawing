ca584074113821ace71956da7b5274ee276ff8ff
antoineErnewein <erneweina@gmail.com>
2014-08-27 11:09:46 +0200

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 (function() {
   var io;
-  io = require('socket.io').listen(4000);
+  io = require('socket.io').listen(80);
   io.sockets.on('connection', function(socket) {
     socket.on('drawClick', function(data) {
       socket.broadcast.emit('draw', {
